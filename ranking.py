890f7fbf85b4cd118b353360f4f6567e498c3b6c
FLAI <59873355+gitflai@users.noreply.github.com>
2021-07-10 20:24:57 -0300

    Add files via upload

--- ranking.py
+++ ranking.py
@@ -1,36 +1,45 @@
 import streamlit as st
 import pandas as pd
 import emoji 
- 
+import st_aggrid as stg 
+
 
 st.set_page_config(page_title = 'Ranking da 4ª Competição de ML FLAI', 
 				   page_icon = 'iconeflai.png' ,
 				   layout = 'centered', 
 				   initial_sidebar_state = 'collapsed')
  
-
-
+  
 st.image('bannerflai.jpg', use_column_width = 'always')
 
-st.markdown('# **Ranking da 4ª Competição de Machine Learning FLAI**')
- 
-st.write('*Última Atualização: 09/07/2021 - 21h00')
-st.markdown('---')
+
+st.markdown('''
+	# Ranking da 4ª Competição de Machine Learning FLAI 
+
+	###### *Última Atualização: 09/07/2021
+	
+	---
+''')
 
 rank = pd.read_csv('4compMLflairank.csv')
 subm = pd.read_csv('4compMLflai.csv')
 
-rank.index = rank['Nome']
-rank.drop(['Nome'], axis = 1, inplace = True)
+
+#rank.index = rank['Nome']
+#rank.drop(['Nome'], axis = 1, inplace = True)
 
 col1, col2 = st.beta_columns(2) 
 
-if col1.button('Clique aqui para ver o RANKING GERAL DA COMPETIÇÃO'):
-	st.table(rank)
+if col1.button('RANKING GERAL DA COMPETIÇÃO'):
+	st.markdown('---') 
+	st.write(rank.to_html(index=False), unsafe_allow_html=True)
 
-if col2.button('Clique aqui para ver a LISTAGEM DE TODAS AS SUBMISSÕES'):
-	st.write(subm)
+if col2.button('LISTAGEM DE TODAS AS SUBMISSÕES'):
+	st.markdown('---') 
+	stg.AgGrid(subm.round(5), height = 800, fit_columns_on_grid_load = True)
 
 st.markdown('---') 
 
-st.image('logoflai.png')
+st.sidebar.image('logoflai.png')
+st.sidebar.markdown('---')
+st.sidebar.markdown('#### Versão 0.1.0')
