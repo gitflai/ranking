5b57cd5a022fdaeb013c89d1bb3994a274ce6521
FLAI <59873355+gitflai@users.noreply.github.com>
2021-09-09 21:06:22 -0300

    Update ranking.py

--- ranking.py
+++ ranking.py
@@ -16,7 +16,7 @@ st.image('bannerflai.jpg', use_column_width = 'always')
 st.markdown('''
 	# Ranking da 5ª Competição de Machine Learning FLAI 
 
-	###### *Última Atualização: 08/09/2021
+	###### *Última Atualização: 09/09/2021
 	
 	---
 ''')
