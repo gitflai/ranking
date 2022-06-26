250cb4d22f6b8184efa2563065f946ec926a7c32
FLAI <59873355+gitflai@users.noreply.github.com>
2022-06-26 20:08:48 -0300

    Update ranking.py

--- ranking.py
+++ ranking.py
@@ -16,7 +16,7 @@ st.image('bannerflai.jpg', use_column_width = 'always')
 st.markdown('''
 	# Ranking da 9ª Competição de Machine Learning FLAI 
 
-	###### NOVA Deadline: 26/06/2022 às 20h!
+	###### COMPETIÇÃO ENCERRADA!
 	---
 ''')
 
